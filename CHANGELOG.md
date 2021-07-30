87ee5a7c1601659b9736e6186edc1d165770f480
Kylian <kylian.barusseau@omedan.com>
2021-07-30 10:10:26 +0200

    WIP: GetMapByKey

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-All notable changes to `:package_name` will be documented in this file.
+All notable changes to `php-beatsaver-api` will be documented in this file.
 
 Updates should follow the [Keep a CHANGELOG](https://keepachangelog.com/) principles.
 
---
+++ lib/BeatSaverAPI.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace BeatSaverAPI;
+
+class BeatSaverAPI
+{
+    const BEATSAVER_URL = "https://beatsaver.com/api";
+
+    private string $userAgent;
+
+    /**
+     * @param string $userAgent User Agent to provide to Beat Saver API
+     */
+    public function __construct(string $userAgent)
+    {
+        $this->userAgent = $userAgent;
+    }
+
+    private function callAPI(string $endpoint)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => self::BEATSAVER_URL . $endpoint,
+            CURLOPT_USERAGENT => $this->userAgent
+        ]);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $result;
+    }
+
+    /**
+     * CALL
+     */
+
+    /**
+     * Get map by BSR Key
+     * @param string $bsrKey BSR Key of the map
+     */
+    public function getMapByKey(string $bsrKey)
+    {
+        var_dump($this->callAPI("/maps/detail/" . $bsrKey));
+        die;
+    }
+}
